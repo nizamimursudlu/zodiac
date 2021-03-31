778a1551435cd5c3c6439437a3e813d7a8660b7a
Nizami Mursudlu <nizamimursudlu@gmail.com>
2021-03-31 10:43:57 +0200

    some work after Jim's review

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 
 async function fetchSigns(sign, day) {
   const url = `https://aztro.sameerkumar.website/?sign=${sign}&day=${day}`;
@@ -6,46 +7,54 @@ async function fetchSigns(sign, day) {
   return json
 }
 
-async function chosenSignDay(data, sign, day) {
-  document.getElementById("entrance-page").textContent = ""
-  document.getElementById("horoscope-result").classList.remove("hide")
-  const backToMenu = document.getElementById("back-to-menu")
-  backToMenu.classList.remove("hide")
-  document.getElementById("horoscope-for-tomorrow").classList.remove("hide")
-  const element = data.current_date;
-
-  const horoscopeText = `
-  <img src="icons/${sign}.jpeg" style="float: left"></img>
-  <h1>You are a ${sign.slice(0, 1).toUpperCase() + sign.slice(1)}</h1> 
-  <h2>${day} is - ${element}</h2> 
-  <div id="match-lucky">
-  <h3>${day}'s Match<br>
-  ${data.compatibility}</h3>
-  <h3>${day}'s Color<br>
-  ${data.color}</h3>
-  <h3>Lucky Number<br>
-  ${data.lucky_number}</h3>
-  <h3>Lucky Time Of The Day<br>
-  ${data.lucky_time}</h3>
-  </div>
-  <p>${data.description}</p>`
-  document.getElementById("horoscope-result").innerHTML = horoscopeText
-
+function createElement(elementType, parent, text) {
+  const element = document.createElement(elementType);
+  parent.appendChild(element).textContent = text
+  return element 
 }
 
-async function main() {
+function displayData(data, sign, day) {
+
+  // hides the first page 'menu' and shows buttons 'back to menu' and 'horoscope for tomorrow'
+  document.getElementById("entrance-page").textContent = ""
+  const horoscopeResult = document.getElementById("horoscope-result")
+  horoscopeResult.classList.remove("hide")
+  horoscopeResult.textContent = ""
+  document.getElementById("back-to-menu").classList.remove("hide")
+  document.getElementById("horoscope-for-tomorrow").classList.remove("hide")
+
+  // displays data for today's or tomorrow's horoscope
+  const img = createElement("img", horoscopeResult)
+  img.src = `icons/${sign}.jpeg`
+  img.style = "float: left"
+  createElement("h1", horoscopeResult, `You are a ${sign.slice(0, 1).toUpperCase() + sign.slice(1)}`)
+  createElement("h2", horoscopeResult, `${day} is - ${data.current_date}`)
+  const matchAndLucky = createElement("div", horoscopeResult)
+  matchAndLucky.id = "match-lucky"
+  createElement("h3", matchAndLucky, `${day}'s Match ${data.compatibility}`)
+  createElement("h3", matchAndLucky, `${day}'s Color ${data.color}`)
+  createElement("h3", matchAndLucky, `Lucky Number ${data.lucky_number}`)
+  createElement("h3", matchAndLucky, `Lucky Time Of The Day ${data.lucky_time}`)
+  createElement("p", horoscopeResult, `${data.description}`)
+}
+
+function main() {
   try {
-  const zodiac = document.getElementsByClassName("zodiac")
-  for (zodiacs of zodiac) {
-    zodiacs.addEventListener("click", async (event) => {  
-        let day = "Today"
-        let sign = event.target.getAttribute("data-value")
-        let data = await fetchSigns(sign, day)
-        chosenSignDay(data, sign, day)
+  const zodiacs = document.getElementsByClassName("zodiac")
+
+  // when clicked on one of the signs(images) in the menu displays horoscope of that sign for today
+  for (const zodiac of zodiacs) {
+    zodiac.addEventListener("click", async (event) => {  
+        const day = "Today"
+        const sign = event.target.getAttribute("data-value")
+        const data = await fetchSigns(sign, day)
+        displayData(data, sign, day)
+
+  // when clicked on button 'horoscope for tomorrow' displays data of the same sign for tomorrow
     document.getElementById("horoscope-for-tomorrow").addEventListener("click", async () =>{
-        let day = "Tomorrow"
-        let data = await fetchSigns(sign, day)
-        chosenSignDay(data, sign, day)
+        const day = "Tomorrow"
+        const data = await fetchSigns(sign, day)
+        displayData(data, sign, day)
         })   
     })
   }
