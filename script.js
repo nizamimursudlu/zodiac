4972881b7fca6bccb465c59f17e410a9f5b8415f
Nizami Mursudlu <nizamimursudlu@gmail.com>
2021-04-01 10:53:28 +0200

    better error handeling

--- script.js
+++ script.js
@@ -1,67 +1,91 @@
-"use strict";
+'use strict';
 
-async function fetchSigns(sign, day) {
-  const url = `https://aztro.sameerkumar.website/?sign=${sign}&day=${day}`;
-  const response = await fetch(url, { method: 'POST' })
-  const json = response.json()
-  return json
+async function fetchData(sign, day) {
+  const url = `https://aztro.sameerkumar.website/?sign=${sign}&${day}`;
+  const response = await fetch(url, { method: 'POST' });
+  const json = await response.json();
+  return json;
 }
 
 function createElement(elementType, parent, text) {
   const element = document.createElement(elementType);
-  parent.appendChild(element).textContent = text
-  return element 
+  parent.appendChild(element).textContent = text;
+  return element;
 }
 
 function displayData(data, sign, day) {
-
   // hides the first page 'menu' and shows buttons 'back to menu' and 'horoscope for tomorrow'
-  document.getElementById("entrance-page").textContent = ""
-  const horoscopeResult = document.getElementById("horoscope-result")
-  horoscopeResult.classList.remove("hide")
-  horoscopeResult.textContent = ""
-  document.getElementById("back-to-menu").classList.remove("hide")
-  document.getElementById("horoscope-for-tomorrow").classList.remove("hide")
+  document.getElementById('entrance-page').classList.add('hide');
+  const horoscopeResult = document.getElementById('horoscope-result');
+  horoscopeResult.classList.remove('hide');
+  horoscopeResult.textContent = '';
+
+  document.getElementById('back-to-menu').classList.remove('hide');
+  document.getElementById('horoscope-for-tomorrow').classList.remove('hide');
+  const backToMenu = document.getElementById('back-to-menu');
+  backToMenu.addEventListener('click', () => {
+    document.getElementById('entrance-page').classList.remove('hide');
+    horoscopeResult.classList.add('hide');
+    backToMenu.classList.add('hide');
+    document.getElementById('horoscope-for-tomorrow').classList.add('hide');
+    // horoscopeResult.textContent = '';
+  });
 
   // displays data for today's or tomorrow's horoscope
-  const img = createElement("img", horoscopeResult)
-  img.src = `icons/${sign}.jpeg`
-  img.style = "float: left"
-  createElement("h1", horoscopeResult, `You are a ${sign.slice(0, 1).toUpperCase() + sign.slice(1)}`)
-  createElement("h2", horoscopeResult, `${day} is - ${data.current_date}`)
-  const matchAndLucky = createElement("div", horoscopeResult)
-  matchAndLucky.id = "match-lucky"
-  createElement("h3", matchAndLucky, `${day}'s Match ${data.compatibility}`)
-  createElement("h3", matchAndLucky, `${day}'s Color ${data.color}`)
-  createElement("h3", matchAndLucky, `Lucky Number ${data.lucky_number}`)
-  createElement("h3", matchAndLucky, `Lucky Time Of The Day ${data.lucky_time}`)
-  createElement("p", horoscopeResult, `${data.description}`)
+  const img = createElement('img', horoscopeResult);
+  img.src = `icons/${sign}.jpeg`;
+  img.style = 'float: left';
+  createElement(
+    'h1',
+    horoscopeResult,
+    `You are a ${sign.slice(0, 1).toUpperCase() + sign.slice(1)}`,
+  );
+  createElement('h2', horoscopeResult, `${day} is - ${data.current_date}`);
+  const matchAndLucky = createElement('div', horoscopeResult);
+  matchAndLucky.id = 'match-lucky';
+  createElement('h3', matchAndLucky, `${day}'s Match ${data.compatibility}`);
+  createElement('h3', matchAndLucky, `${day}'s Color ${data.color}`);
+  createElement('h3', matchAndLucky, `Lucky Number ${data.lucky_number}`);
+  createElement(
+    'h3',
+    matchAndLucky,
+    `Lucky Time Of The Day ${data.lucky_time}`,
+  );
+  createElement('p', horoscopeResult, `${data.description}`);
 }
 
 function main() {
-  try {
-  const zodiacs = document.getElementsByClassName("zodiac")
+  const zodiacs = document.getElementsByClassName('zodiac');
 
   // when clicked on one of the signs(images) in the menu displays horoscope of that sign for today
   for (const zodiac of zodiacs) {
-    zodiac.addEventListener("click", async (event) => {  
-        const day = "Today"
-        const sign = event.target.getAttribute("data-value")
-        const data = await fetchSigns(sign, day)
-        displayData(data, sign, day)
+    const showError = document.getElementById('show-error');
+    const entrancePage = document.getElementById('entrance-page');
+    zodiac.addEventListener('click', (event) => {
+      let day = 'Today';
+      const sign = event.target.getAttribute('data-value');
+      fetchDisplayCatch(showError, entrancePage, day, sign);
 
-  // when clicked on button 'horoscope for tomorrow' displays data of the same sign for tomorrow
-    document.getElementById("horoscope-for-tomorrow").addEventListener("click", async () =>{
-        const day = "Tomorrow"
-        const data = await fetchSigns(sign, day)
-        displayData(data, sign, day)
-        })   
-    })
+      // when clicked on button 'horoscope for tomorrow' displays data of the same sign for tomorrow
+      document
+        .getElementById('horoscope-for-tomorrow')
+        .addEventListener('click', () => {
+          day = 'Tomorrow';
+          fetchDisplayCatch(showError, entrancePage, day, sign);
+        });
+    });
   }
-}   catch (error) {
-        console.log(error)
-      }
-    
 }
 
-window.onload = main
+async function fetchDisplayCatch(showError, entrancePage, day, sign) {
+  try {
+    const data = await fetchData(sign, day);
+    displayData(data, sign, day);
+  } catch (error) {
+    entrancePage.classList.add('hide');
+    showError.classList.remove('hide');
+    showError.textContent = error;
+  }
+}
+
+window.onload = main;
